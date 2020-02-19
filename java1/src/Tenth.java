474d13404ae3d89df5804973a984573eeb015d25
kshitija <kshitija.verma@tothenew.com>
2020-02-19 15:43:44 +0530

    committed final

--- java1/src/Tenth.java
+++ java1/src/Tenth.java
@@ -1,7 +1,7 @@
 public class Tenth {
-    void Add(int a , int b)
+    void Add(Integer a , Integer b)
     {
-        int c = a +b;
+        Integer c = a +b;
         System.out.println("Sum of integer : "+c);
 
     }
@@ -18,7 +18,7 @@ public class Tenth {
     void Multiply(int a , int b)
     {
         float c = a*b;
-        System.out.println("Multiplication of integer: "+ c);
+        System.out.println("Multiplication of int: "+ c);
     }
     void Concate(String s1,String s2)
     {
@@ -35,7 +35,10 @@ public class Tenth {
 
     public static void main(String[] args) {
         Tenth t = new Tenth();
-        t.Add(5,6);
+        Integer a = new Integer(5);
+        Integer b = new Integer(6);
+
+        t.Add(a,b);
         t.Add(5.6,8.7);
         t.Multiply(4,6);
         t.Multiply(3.4f,5.4f);
