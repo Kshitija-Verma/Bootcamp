705c71ec206e830e50782aaab53f351c0b3481be
kshitija <kshitija.verma@tothenew.com>
2020-03-09 12:57:49 +0530

    Restful assignment part 2 committed

---
+++ RestfulApp2/src/main/java/com/RestfulApp2/RestfulApp2/RestfulApp2Application.java
@@ -0,0 +1,13 @@
+package com.RestfulApp2.RestfulApp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApp2Application.class, args);
+	}
+
+}
---
+++ RestfulApp2/src/test/java/com/RestfulApp2/RestfulApp2/RestfulApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestfulApp2.RestfulApp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
