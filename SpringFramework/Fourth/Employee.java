94f518397b8dcfe72b4baf28f8c312fde6b5521d
kshitija <kshitija.verma@tothenew.com>
2020-03-13 09:17:14 +0530

    spring framework committed

--- SpringFramework/Fourth/Employee.java
+++
@@ -1,6 +0,0 @@
-package Third;
-
-public interface Employee {
-    public void doWork();
-    public void doingRest();
-}
--- SpringFramework/demo/src/main/java/com/tothenew/bootcamp/demo/Third.java
+++ SpringFramework/demo/src/main/java/com/tothenew/bootcamp/demo/Third.java
@@ -1,16 +1,25 @@
-package Third;
+package com.tothenew.bootcamp.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
+interface Employee{
+    public void doWork();
+}
+
+@Component
+class Trainer{
+    @Autowired
+    private Employee employee;
+
+}
 @SpringBootApplication
 public class Third {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(Third.class,args);
-       Trainer trainer = applicationContext.getBean(Trainer.class);
-       trainer.manageWork();
-       trainer.leaveManage();
 
     }
 }
