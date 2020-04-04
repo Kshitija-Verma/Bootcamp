6250115a8aa728cb213891cf5bfcea910ab565ba
Kshitija Verma <60641013+Kshitija-Verma@users.noreply.github.com>
2020-04-04 14:50:53 +0530

    Update Apple.java

--- SpringFramework/demo/src/main/java/Sixth/Apple.java
+++ SpringFramework/demo/src/main/java/Sixth/Apple.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Apple {
+    @Autowired
+    public Apple(){}
     public void display(){
         System.out.println("This is an apple");
     }
